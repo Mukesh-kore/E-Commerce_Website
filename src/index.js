0db7b9a75f5752e16beadf5f80954e490ecb7f08
Mukesh Kore <koremukesh9@gmail.com>
2023-06-23 14:13:28 +0530

    changes in UI

--- src/index.js
+++ src/index.js
@@ -3,31 +3,31 @@ import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css"
-import App from './App';
-import RegisterComponent from './components/RegisterComponents';
-import LoginComponent from './components/LoginComponent';
-import FormComponent from './components/formComponent';
-import FormikComponent from './components/formikDemo';
-import ValidationComponent from './components/FormValidation_component'
-import YupValidation from './components/YupValidation';
-import FetchApiComponent from './components/FetchApi';
-import SyntheticEventHandler from './components/syntheticEvent';
+// import App from './App';
+// import RegisterComponent from './components/RegisterComponents';
+// import LoginComponent from './components/LoginComponent';
+// import FormComponent from './components/formComponent';
+// import FormikComponent from './components/formikDemo';
+// import ValidationComponent from './components/FormValidation_component'
+// import YupValidation from './components/YupValidation';
+// import FetchApiComponent from './components/FetchApi';
+// import SyntheticEventHandler from './components/syntheticEvent';
 // import AxiosComponent from './components/AxiosComponent';
-import KeyImportance from './components/Keys_importance';
-import RouteComponent from './components/RouteComponent';
-import IndexComponent from './components/AmazonSite/IndexComponent';
-import Login from './components/UseCookieDemo';
-import ContextDemoComponent from './components/UseContextComponent';
-import UseStateComponent from './components/useStateComponent';
-import UseReducerComponent from './components/UseReducerComponent';
+// import KeyImportance from './components/Keys_importance';
+// import RouteComponent from './components/RouteComponent';
+// // import IndexComponent from './components/AmazonSite/IndexComponent';
+// import Login from './components/UseCookieDemo';
+// import ContextDemoComponent from './components/UseContextComponent';
+// import UseStateComponent from './components/useStateComponent';
+// import UseReducerComponent from './components/UseReducerComponent';
 
-import DemoState from './components/state';
-import LifeCycleComponent from './components/AmazonSite/LifeCycleComponent';
-import UseEffectComponent from './components/UseEffectComponent';
+// import DemoState from './components/state';
+// // import LifeCycleComponent from './components/AmazonSite/LifeCycleComponent';
+// import UseEffectComponent from './components/UseEffectComponent';
 
 // import store from "./store";
 // import ReducerComponent from './ReducerComponent';
-// import './reducer.css'
+import './reducer.css'
 import { Provider } from "react-redux";
 
 
@@ -36,12 +36,12 @@ import store_redux_pro from "./redux_pro/store_redux_pro"
 import ReduxProComponent from './redux-pro_component';
 import './redux_pro_style.css'
 import reportWebVitals from './reportWebVitals';
-import { Form } from 'formik';
-import UseEffectPrimaryComponent from './components/useEffectPrimaryComponent';
-import UseRefComponent from './components/useRefComponent';
-import UseMemoComponent from './components/useMemoComponent';
-import UseCallBackComponent from './components/UseCallBaackComponent';
-import AxiosPracticeComponent from './components/AxiosPracticeComponent';
+// import { Form } from 'formik';
+// import UseEffectPrimaryComponent from './components/useEffectPrimaryComponent';
+// import UseRefComponent from './components/useRefComponent';
+// import UseMemoComponent from './components/useMemoComponent';
+// import UseCallBackComponent from './components/UseCallBaackComponent';
+// import AxiosPracticeComponent from './components/AxiosPracticeComponent';
 const comp = ReactDOM.createRoot(document.getElementById('root'));
 comp.render(
  
